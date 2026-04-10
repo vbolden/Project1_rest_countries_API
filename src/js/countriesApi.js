3dc3565271aae8c855f842db370cc31ba21d5f49
Valerie Bolden <valerie.bolden@gmail.com>
2026-04-10 12:59:21 -0400

    upgraded fetch url, added logic to the search function

--- src/js/countriesApi.js
+++ src/js/countriesApi.js
@@ -9,7 +9,7 @@ window.addEventListener("load", getCountries)
 // API FETCH FUNCTION
 async function getCountries() {
     try {
-        const response = await fetch("https://restcountries.com/v3.1/all");
+        const response = await fetch("https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital");
         const data = await response.json();
 
         countriesData = data;
@@ -33,13 +33,13 @@ function renderCountries(data) {
         let capital = country.capital ? country.capital[0] : "N/A";
 
         let card = document.createElement("div")
-        card.className = "col-md-4 mb-4"
+        card.className = "col-12 col-md-6 col-lg-3 mb-4"
         card.innerHTML = `
-        <div class="card" style="width: 18rem;">
-            <img src="${flag}" class="card-img-top" alt="${name}'s flag">
+        <div class="card h-100 d-flex flex-column">
+            <img src="${flag}" class="card-img-top country-flag" alt="${name}'s flag">
             <div class="card-body">
                 <h5 class="card-title">${name}</h5>
-                <p class="card-text">Population: ${population.toLocalString()}</p>
+                <p class="card-text">Population: ${population.toLocaleString()}</p>
                 <p class="card-text">Region: ${region}</p>
                 <p class="card-text">Capital: ${capital}</p>
             </div>
@@ -53,5 +53,14 @@ function renderCountries(data) {
 searchInput.addEventListener("input", () => {
     const searchValue = searchInput.value.toLowerCase();
 
-    
+    if (searchValue === "") {
+        renderCountries(countriesData);
+        return;
+    }
+
+    const filteredCountries = countriesData.filter(country =>
+        country.name.common.toLowerCase().includes(searchValue)
+    )
+
+    renderCountries(filteredCountries);
 })
